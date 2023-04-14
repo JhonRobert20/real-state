143a63a2f21a4a7c7672ee006c2135e050926c17
Jhon Robert <jhonbara51214@gmail.com>
2023-04-14 21:55:07 +0200

    feat: cors and static root

--- real_state/real_state/settings.py
+++ real_state/real_state/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 env_path = Path(".") / ".env"
 load_dotenv(dotenv_path=env_path)
@@ -116,7 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -126,3 +127,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 API_V1_STR: str = "/api/fa/v1"
 WSGI_APP_URL: str = "/web"
 PROJECT_NAME: str = "Real State"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
