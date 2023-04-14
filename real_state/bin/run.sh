a284594da6760f9c3dc3b212e908fc66084f4ccd
Jhon Robert <jhonbara51214@gmail.com>
2023-04-14 20:31:00 +0200

    feat: run django and fast api

--- real_state/bin/run.sh
+++ real_state/bin/run.sh
@@ -6,17 +6,10 @@ set -m
 # collect static files in /static_root
 python manage.py collectstatic --noinput
 
-# wait until DB is ready
-python manage.py wait_for_db
 
 # DB migrations
 python manage.py migrate
 
-# run Django and put it in the background
-python manage.py runserver 0.0.0.0:8000 &
-
-# start uwsgi
-uwsgi --ini uwsgi.ini:local --uid=nobody --gid=nogroup --env=DJANGO_SETTINGS_MODULE=real_state.settings &
-
-# bring the primary process back into the foreground and leave it there
+# Run django and fast api
+gunicorn real_state.asgi:app -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 --reload &
 fg %1
