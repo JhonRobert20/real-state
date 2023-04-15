e64c62f81d77001c1082ab163d5d9b913b2ec0b4
Jhon Robert <jhonbara51214@gmail.com>
2023-04-15 20:23:24 +0200

    feat: new fake endpoint - just for demo

--- real_state/estates/endpoints.py
+++ real_state/estates/endpoints.py
@@ -1,13 +1,22 @@
 from typing import Any
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+
+from real_state.auth import is_logged
 
 router = APIRouter()
 
 
 @router.get("/estates/", response_model=str)
-def get_multiple_estate(offset: int = 0, limit: int = 10) -> Any:
+def get_multiple_estate() -> Any:
     """
-    Endpoint to get multiple posts based on offset and limit values.
+    Endpoint to get multiple real states based on offset and limit values.
     """
+    return "Welcome to the real state fastapi app"
+
+
+@router.get("/items/")
+async def read_confidential_info(logged: bool = Depends(is_logged)):
+    if not logged:
+        return HTTPException(status_code=401, detail="Not logged")
     return "Welcome to the real state fastapi app"
