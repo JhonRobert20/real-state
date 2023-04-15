12b4b1109979465b2eaae783bd52a1f746707913
Jhon Robert <jhonbara51214@gmail.com>
2023-04-15 23:12:28 +0200

    feat: test for populate command

---
+++ real_state/estates/tests/tests_models.py
@@ -0,0 +1,14 @@
+import pytest
+from django.core.management import call_command
+from django.test import TestCase
+from users.models import User
+
+from real_state.mongodb import mongodb
+
+
+@pytest.mark.django_db
+class EstateBaseTest(TestCase):
+    def test_populate_command(self):
+        call_command("populate")
+        assert User.objects.count() == 10
+        assert len(list(mongodb.filter_estates({}))) == 11
