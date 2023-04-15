3dec1ee174c7af6e9ef6e95c5a6f8249370fa4c4
Jhon Robert <jhonbara51214@gmail.com>
2023-04-15 16:16:17 +0200

    refactor: remove base app

--- real_state/base/exceptions.py
+++
@@ -1,6 +0,0 @@
-class ValidationError(Exception):
-    """The django validation error works with a list of errors, but we want to
-    work with a single error, so we create a new exception to handle this
-    """
-
-    pass
