fdb43d7f0a27df1a57cef32727a8f1ad6bad1eea
Jhon Robert <jhonbara51214@gmail.com>
2023-04-15 20:21:59 +0200

    feat: add cookie user id to get the logged user

--- real_state/users/views.py
+++ real_state/users/views.py
@@ -1,10 +1,30 @@
+import datetime
 from time import sleep
 
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import redirect, render
 from users.forms import NewUserForm
+
+
+def set_cookie(response, key, value, days_expire=7):
+    one_day = 24 * 60 * 60
+    max_age = days_expire * one_day if days_expire is not None else one_day
+
+    expires = datetime.datetime.strftime(
+        datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age),
+        "%a, %d-%b-%Y %H:%M:%S GMT",
+    )
+    response.set_cookie(
+        key,
+        value,
+        max_age=max_age,
+        expires=expires,
+        domain=settings.SESSION_COOKIE_DOMAIN,
+        secure=settings.SESSION_COOKIE_SECURE or None,
+    )
 
 
 def register_request(request):
@@ -14,11 +34,12 @@ def register_request(request):
         form = NewUserForm(request.POST)
         if form.is_valid():
             user = form.save()
-
+            response = redirect("home")
+            set_cookie(response, "user_id", str(request.user.id))
             login(request, user)
             messages.success(request, "Registration successful.")
             sleep(0.3)
-            return redirect("home")
+            return response
 
         messages.error(request, "Unsuccessful registration. Invalid information.")
     form = NewUserForm()
@@ -38,8 +59,10 @@ def login_request(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
+                response = redirect("home")
+                set_cookie(response, "user_id", str(request.user.id))
                 messages.info(request, f"You are now logged in as {username}.")
-                return redirect("home")
+                return response
             else:
                 messages.error(request, "Invalid email or password.")
         else:
@@ -55,6 +78,8 @@ def homepage(request):
 
 
 def logout_request(request):
+    response = redirect("home")
     logout(request)
+    response.delete_cookie("user_id")
     messages.info(request, "You have successfully logged out.")
-    return redirect("home")
+    return response
