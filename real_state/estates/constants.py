55724cc7607c2fdeffc620f296de56fb6c4e711b
Jhon Robert <jhonbara51214@gmail.com>
2023-04-16 00:17:20 +0200

    feat: new fake estate constant

--- real_state/estates/constants.py
+++ real_state/estates/constants.py
@@ -1,1 +1,21 @@
 ESTATE_COLLECTION_NAME = "estates"
+
+fake_estate = {
+    "build_status": "1.0",
+    "is_active": False,
+    "start_month": 19212,
+    "construction_type": 2,
+    "date_diff": 2,
+    "description": "-CABANILLAS DEL CAMPO/GUADALAJARA- ",
+    "date_in": "2021-05-31T00:00:00+00:00",
+    "property_type": 3,
+    "end_week": 15186,
+    "typology_type": 4,
+    "id": "6124016",
+    "coordinates": "40.643200000000000,-3.237410000000000120934",
+    "boundary_id": "64607.0",
+    "id_uda": "54-94183090",
+    "title": "Alquiler de Casa o chalet en camino de las Peñas — idealista",
+    "listing_type": 1,
+    "date": "2021-06-14T00:00:00+00:00",
+}
