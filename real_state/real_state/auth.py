e25d3cea02ad857393176cef730c7e48a3d7bf47
Jhon Robert <jhonbara51214@gmail.com>
2023-04-16 13:07:31 +0200

    refactor: separate the logic for mocking purposes

--- real_state/real_state/auth.py
+++ real_state/real_state/auth.py
@@ -2,12 +2,16 @@ from fastapi import HTTPException, Request
 from users.models import User
 
 
+def check_exists_user(user_id):
+    exists_user = User.objects.filter(id=user_id).exists()
+    return exists_user
+
+
 def is_logged(request: Request):
     cookies = request.cookies
     user_id = cookies.get("user_id")
     if not user_id:
         raise HTTPException(status_code=401, detail="Not logged")
 
-    exists_user = User.objects.filter(id=user_id).exists()
-    if not exists_user:
+    if not check_exists_user(user_id):
         raise HTTPException(status_code=401, detail="Not logged")
