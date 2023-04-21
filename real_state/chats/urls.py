cc7c7743c307b0acb925111bfe95ada5a0450436
Jhon Robert <jhonbara51214@gmail.com>
2023-04-21 11:57:07 +0200

    refactor: chat need the chat id

--- real_state/chats/urls.py
+++ real_state/chats/urls.py
@@ -2,7 +2,7 @@ from chats import views
 from django.urls import path
 
 urlpatterns = [
-    path("chat", views.chat_page, name="chat"),
+    path("chat/<str:chat_id>/", views.chat_page, name="chat"),
     path("", views.chat_home_page, name="chat_home"),
     path("new_chat", views.new_chat_request, name="new_chat"),
     path("new_message", views.new_message_request, name="new_message"),
